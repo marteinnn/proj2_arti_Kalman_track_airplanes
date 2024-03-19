3fb206519aba083d22dcfe3d31de5ac2b694857f
marteinnn <marteinnh22@ru.is>
2024-03-19 11:49:50 +0000

    plot, task 1 + 2 finished?

--- project2/project2.py
+++ project2/project2.py
@@ -4,9 +4,9 @@ from project2_base import *
 from filterpy.kalman import KalmanFilter
 from src.kalmanfilter import create_kalman_filter
 import numpy as np
-
-#############################
-
+from traffic.data.samples import belevingsvlucht
+from rich.console import Console
+import matplotlib.pyplot as plt
 
 def main():
     # Create Kalman filter
@@ -15,7 +15,7 @@ def main():
     # Simulate some data
     true_state = np.array([0, 1, 0, 1])  # [x, vx, y, vy]
     measurement_noise = np.random.normal(0, 100, 2)  # [x, y]
-    measurement = kf.H @ true_state + measurement_noise
+    measurement = kf.H @ true_state + measurement_noise  # Use 'H'
 
     # Predict next state
     kf.predict()
@@ -25,8 +25,21 @@ def main():
 
     print("Estimated state:", kf.x)
 
+    # Print the sample flight
+    print(belevingsvlucht)
 
-#############################
+    # Print the flight with rich representation
+    console = Console()
+    console.print(belevingsvlucht)
+
+    # Print the first 30 minutes of the flight
+    print(belevingsvlucht.first(minutes=30))
+
+    # Plot the flight
+    fig, ax = plt.subplots()
+    belevingsvlucht.plot(ax)
+
+    plt.show()  # Display the plot
 
 if __name__ == "__main__":
     main()
